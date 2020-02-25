8700d32b6d5788265ad2086f50f4ad8043b81f79
Elan <elanpodo@gmail.com>
2020-02-25 16:39:18 -0700

    Color sensor code
    test for color sensor code

---
+++ src/main/java/frc/robot/ColorMatch.java
@@ -0,0 +1,18 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.util.Color;
+
+public class ColorMatch {
+
+	public static Color makeColor(double d, double e, double f) {
+		return null;
+	}
+
+	public void addColorMatch(Color kBlueTarget) {
+	}
+
+	public ColorMatchResult matchClosestColor(Color detectedColor) {
+		return null;
+	}
+
+}
---
+++ src/main/java/frc/robot/ColorMatchResult.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.util.Color;
+
+public class ColorMatchResult {
+
+	public Color color;
+	public double confidence;
+
+}
