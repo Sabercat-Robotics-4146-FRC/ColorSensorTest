7c824bcc9701f9ff154bf182781ce21081b67642
lucas lesser <lesser618@gmail.com>
2020-02-27 16:49:55 -0700

    Color Wheel Code
    Spins wheel 4 times, color match in progress

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,5 +1,3 @@
-package frc.robot;
-
 /*----------------------------------------------------------------------------*/
 /* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
@@ -7,22 +5,44 @@ package frc.robot;
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
+package frc.robot;
 
-import edu.wpi.first.wpilibj.I2C;
+
+
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.DriverStation;
 
+import java.text.BreakIterator;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorMatch;
+import com.revrobotics.CANSparkMax.IdleMode;
 /**
  * This is a simple example to show how the REV Color Sensor V3 can be used to
  * detect pre-configured colors.
  */
+
+
 public class Robot extends TimedRobot {
   /**
    * Change the I2C port below to match the connection of your color sensor
    */
   private final I2C.Port i2cPort = I2C.Port.kOnboard;
-
+  private CANSparkMax motorController;
+  private int counter = 0;
+  private boolean blueColorFlag = false;
+  private String gameData;
+  
+  
   /**
    * A Rev Color Sensor V3 object is constructed with an I2C port as a 
    * parameter. The device will be automatically initialized with default 
@@ -34,15 +54,16 @@ public class Robot extends TimedRobot {
    * A Rev Color Match object is used to register and detect known colors. This can 
    * be calibrated ahead of time or during operation.
    * 
-   * This object uses a simple euclidian distance to estimate the closest match
+   * This object uses a simple euclidean distance to estimate the closest match
    * with given confidence range.
    */
   private final ColorMatch m_colorMatcher = new ColorMatch();
-
+  private Joystick driverController;
   /**
    * Note: Any example colors should be calibrated as the user needs, these
    * are here as a basic example.
    */
+
   private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
   private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
   private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
@@ -54,6 +75,8 @@ public class Robot extends TimedRobot {
     m_colorMatcher.addColorMatch(kGreenTarget);
     m_colorMatcher.addColorMatch(kRedTarget);
     m_colorMatcher.addColorMatch(kYellowTarget);    
+    driverController = new Joystick(0);
+    motorController = new CANSparkMax(3, MotorType.kBrushless);
   }
 
   @Override
@@ -69,7 +92,6 @@ public class Robot extends TimedRobot {
      * measurements and make it difficult to accurately determine its color.
      */
     Color detectedColor = m_colorSensor.getColor();
-
     /**
      * Run the color match algorithm on our detected color
      */
@@ -87,7 +109,8 @@ public class Robot extends TimedRobot {
     } else {
       colorString = "Unknown";
     }
-
+    
+    
     /**
      * Open Smart Dashboard or Shuffleboard to see the color detected by the 
      * sensor.
@@ -97,45 +120,59 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("Blue", detectedColor.blue);
     SmartDashboard.putNumber("Confidence", match.confidence);
     SmartDashboard.putString("Detected Color", colorString);
-
-       /**
-     * The method GetColor() returns a normalized color value from the sensor and can be
-     * useful if outputting the color to an RGB LED or similar. To
-     * read the raw color, use GetRawColor().
-     * 
-     * The color sensor works best when within a few inches from an object in
-     * well lit conditions (the built in LED is a big help here!). The farther
-     * an object is the more light from the surroundings will bleed into the 
-     * measurements and make it difficult to accurately determine its color.
-     */
-
-    /**
-     * The sensor returns a raw IR value of the infrared light detected.
-     */
-    double IR = m_colorSensor.getIR();
-
-    /**
-     * Open Smart Dashboard or Shuffleboard to see the color detected by the 
-     * sensor.
-     */
-    SmartDashboard.putNumber("Red", detectedColor.red);
-    SmartDashboard.putNumber("Green", detectedColor.green);
-    SmartDashboard.putNumber("Blue", detectedColor.blue);
-    SmartDashboard.putNumber("IR", IR);
-
-    /**
-     * In addition to RGB IR values, the color sensor can also return an 
-     * infrared proximity value. The chip contains an IR led which will emit
-     * IR pulses and measure the intensity of the return. When an object is 
-     * close the value of the proximity will be large (max 2047 with default
-     * settings) and will approach zero when the object is far away.
-     * 
-     * Proximity can be used to roughly approximate the distance of an object
-     * or provide a threshold for when an object is close enough to provide
-     * accurate color values.
-     */
-    int proximity = m_colorSensor.getProximity();
-
-    SmartDashboard.putNumber("Proximity", proximity);
+    SmartDashboard.putNumber("Counter", counter);
+    
+    if(driverController.getRawButton(1)) {
+      motorController.set(0.1);
+    }else{
+      motorController.set(0);
+    }
+    if(colorString.equals("Blue") && !blueColorFlag){
+      blueColorFlag = true;
+      counter += 1;
+    }
+    if((colorString.equals("Blue") != true) && blueColorFlag){
+     blueColorFlag = false;
+    }
+    if(counter >= 8 && counter <= 10){
+      motorController.set(0);
+    }
+    if(!(driverController.getRawButton(1))){
+      motorController.setIdleMode(IdleMode.kBrake);
+    }
+gameData = DriverStation.getInstance().getGameSpecificMessage();
+if(gameData.length() > 0)
+{
+  switch (gameData.charAt(0))
+  {
+    case 'B' :
+    ColorMatch.makeColor(0.143, 0.427, 0.429);
+      break;
+    case 'G' :
+    ColorMatch.makeColor(0.197, 0.561, 0.240);
+      break;
+    case 'R' :
+    ColorMatch.makeColor(0.561, 0.232, 0.114);
+      break;
+    case 'Y' :
+    ColorMatch.makeColor(0.361, 0.524, 0.113);
+      break;
+    default :
+      //This is corrupt data
+      break;
+  }
+} else {
+  SmartDashboard.putString("No Color Yet", colorString);
+}
+if(gameData.equals('B')){
+   motorController.set(0.1);
+   if(colorString.equals("Blue")){
+     motorController.set(0);
+   }else{
+     motorController.set(0);
+   }
+}else{
+  motorController.set(0);
+}
   }
 }
